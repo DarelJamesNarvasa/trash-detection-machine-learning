cdf77904fbe0aa2e0ba22445f29d6beeabdeb97a
Darel James A. Narvasa <124035782+DarelJamesNarvasa@users.noreply.github.com>
2025-11-21 11:37:17 +0800

    trash detection

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ def main():
 
     results = model.train(
         data='FinalTrashScan.yolov8/data.yaml',
-        epochs=10,
+        epochs=250,
         imgsz=640,
+        patience=50,
+        batch=16,
+        cache=True,
+        optimizer='AdamW',
+        lr=0.0005,
+        dropout=0.1,
+
         device='cpu',
-        workers=0
+        workers=0,
     )
 
 if __name__ == '__main__':
